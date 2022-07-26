e4a779f40c3ab180d2fbabb80d9916902a64150f
Jonathan Almeida <almeidajonathaan@gmail.com>
2022-07-26 20:15:38 -0300

    projeto landing page responsiva

--- juiceables/assets/js/main.js
+++ juiceables/assets/js/main.js
@@ -6,6 +6,7 @@ const navClose = document.getElementById("nav-close");
 if(navToggle) {
     navToggle.addEventListener("click", () => {
         navMenu.classList.add("show-menu");
+        console.log("clicou abrir");
     })
 }
 
@@ -13,5 +14,34 @@ if(navToggle) {
 if(navClose) {
     navClose.addEventListener("click", () => {
         navMenu.classList.remove("show-menu")
+        console.log("clicou fechar");
+
     })
 }
+
+//Remove menu Mobile
+const navLink = document.querySelectorAll(".nav-link");
+
+function linkAction() {
+    const navMenu = document.getElementById("nav-menu");
+    // when we click on each nav-link, we remove the show-menu class
+    navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach(n => n.addEventListener("click", linkAction));
+
+// GSAP Animation
+TweenMax.from('.home-title', 1, {delay: .2, opacity: 0, y: 20, ease: Expo.easeInOut})
+TweenMax.from('.home-description', 1, {delay: .3, opacity: 0, y: 20, ease: Expo.easeInOut})
+TweenMax.from('.home-button', 1, {delay: .4, opacity: 0, y: 20, ease: Expo.easeInOut})
+TweenMax.from('.home-liquid', 1, {delay: .7, opacity: 0, y: 200, ease: Expo.easeInOut})
+TweenMax.from('.home-juice-animate', 1, {delay: 1.2, opacity: 0, y: -800, ease: Expo.easeInOut})
+TweenMax.from('.home-apple1', 1, {delay: 1.5, opacity: 0, y: -800, ease: Expo.easeInOut})
+TweenMax.from('.home-apple2', 1, {delay: 1.6, opacity: 0, y: -800, ease: Expo.easeInOut})
+TweenMax.from('.home-leaf:nth-child(1)', 2, {delay: 1.3, opacity: 0, y: -800, ease: Expo.easeInOut})
+TweenMax.from('.home-leaf:nth-child(2)', 2, {delay: 1.4, opacity: 0, y: -800, ease: Expo.easeInOut})
+TweenMax.from('.home-leaf:nth-child(3)', 2, {delay: 1.5, opacity: 0, y: -800, ease: Expo.easeInOut})
+TweenMax.from('.home-leaf:nth-child(4)', 2, {delay: 1.6, opacity: 0, y: -800, ease: Expo.easeInOut})
+TweenMax.from('.home-leaf:nth-child(5)', 2, {delay: 1.7, opacity: 0, y: -800, ease: Expo.easeInOut})
+TweenMax.from('.home-leaf:nth-child(6)', 2, {delay: 1.8, opacity: 0, y: -800, ease: Expo.easeInOut})
+
